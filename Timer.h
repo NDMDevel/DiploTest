9004a71f4d291c8391f0b1256979772254983dce
Damian <damian.n.marasco@gmail.com>
2021-11-19 19:30:50 -0300

    Desarrollo class Timer

--- Timer.h
+++ Timer.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <Arduino.h>
+#include <stdint.h>
+
+class Timer
+{
+private:
+    uint32_t tick;
+public:
+    void start()
+    {
+        tick = ~millis() + 1;
+    }
+    uint32_t elapsed()
+    {
+        return tick + millis();
+    }
+};
