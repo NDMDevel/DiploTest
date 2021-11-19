e4602053005c97df46b0d152acc006060e6e5d4f
Damian <damian.n.marasco@gmail.com>
2021-11-19 19:05:07 -0300

    Agrego codigo para led que parpadea

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 //example Arduino
 
+#include <Led.h>
+
+Led led;
+
 void setup()
 {
-
+    led.begin(2);
 }
 
 void loop()
 {
-
+    delay(100);
+    led.on();
+    delay(100);
+    led.off();
 }
