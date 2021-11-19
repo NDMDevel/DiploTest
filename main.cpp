4aa2de22b60945b06c9afc329df7d3d2c872ba8a
Damian <damian.n.marasco@gmail.com>
2021-11-19 19:33:39 -0300

    Uso timer en main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 //example Arduino
+#include "Timer.h"
+
+Timer timer;
 
 void setup()
 {
-
+    timer.start();
+    pinMode(2,OUTPUT);
 }
 
 void loop()
 {
-
+    if( timer.elapsed() > 100 )
+    {
+        bool state = digitalRead(2);
+        digitalWrite(2,!state);
+        timer.start();
+    }
 }
